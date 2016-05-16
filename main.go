adc7347a035b98a356237ceb5cc6f43d84f9ce35
Sage Thomas <sage.thomas@outlook.com>
2016-05-16 10:07:20 -0700

    got templates working

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
+    "text/template"
+    "path/filepath"
+    "sync"
 )
 
 // templ represents a single template
@@ -20,18 +23,8 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 } 
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-            <html>
-                <head>
-                    <title>Chat</title>
-                </head>
-                <body>
-                    Let's chat!
-                </body>
-            </html>
-        `))
-	})
+    // root
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 	// start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
